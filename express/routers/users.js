5e322247e4c3af27f23b6736e67c9ce4b567d0cc
Vishwaraj <sali.vishwaraj@gmail.com>
2020-04-11 17:19:01 -0500

    login router

---
+++ express/routers/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/login', (req, res, next) =>{
+    //if(err) throw err;
+    res.send(
+        {msg : 'login'}
+        )
+})
+
+
+module.exports = router;
