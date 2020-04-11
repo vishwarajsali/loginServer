930a1f44a0627c760c98c42ffd7b0d5805b14a9b
Vishwaraj <sali.vishwaraj@gmail.com>
2020-04-11 17:33:33 -0500

    server

--- express/server.js
+++ express/server.js
@@ -13,19 +13,16 @@ const router = express.Router();
 app.use(bodyParser.json());
 
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
    res.json({msg: "Vish login API Home"});
 })
 
+app.use('/users', users);
+
+//app.use('/.netlify/functions/server/users', users);
+
+
+//app.use('/.netlify/functions/server', router);  // path must route to lambda
  
-
-app.use('/.netlify/functions/server/users', users);
-
-
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/users', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
-
 module.exports = app;
 module.exports.handler = serverless(app);
