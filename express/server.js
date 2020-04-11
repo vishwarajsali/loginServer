fa5c14b83160cf10eb6cbed8c4a5433ea153b28b
Vishwaraj <sali.vishwaraj@gmail.com>
2020-04-11 17:21:43 -0500

    b

--- express/server.js
+++ express/server.js
@@ -21,7 +21,7 @@ app.use('/users', users);
 
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
